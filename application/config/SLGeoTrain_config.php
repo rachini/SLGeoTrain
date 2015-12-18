e0454a335c656660aa9ad2b9c3bb42231d205181
rachini <rachini94perera@gmail.com>
2015-12-18 23:39:33 +0530

    startup

---
+++ application/config/SLGeoTrain_config.php
@@ -0,0 +1,23 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//Custom configuration file.
+
+
+
+//This is to set the site title
+$config['APPLICATION_MAIN_TITLE']	= "SLGeoTrain ";
+$config['LOGIN_OPTION']	= 1;
+
+
+//login Type
+$config['ADMIN']= 1;
+
+
+$config['MAILBOX']             = "{203.143.14.246:143/notls}";
+$config['MAILBOX2']            = "{203.143.14.229:143/notls}";
+
+
+
+
+//system codes where even if the user has no privileges the system default view should be shown
+$config['DEFAULT_VIEW_SYSTEMS'] = array(5,6,7,15);
+
